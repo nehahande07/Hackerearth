8819b3339bb3679e0991d1400b27a49555d212a2
Neha Hande <nehahande07@gmail.com>
2021-02-10 16:30:58 +0530

    Hackerearth- I Am Easy problem solution

---
+++ hackererathIAMEasyProblem.py
@@ -0,0 +1,14 @@
+n = int(input())
+for i in range(1,11):
+   print(n*i)
+
+
+
+# Input:
+
+# An integer n, whose first 10 multiples need to be printed.
+
+# Output:
+
+# First 10 multiples of number given in input
+#https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/i-am-easy/description/
